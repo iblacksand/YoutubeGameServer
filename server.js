beaedc733ae0a7ecd5d62e37669580d44afe9bea
John Elizarraras <chess4j@gmail.com>
2018-01-12 09:36:13 -0700

    adds host and player

--- server.js
+++ server.js
@@ -4,24 +4,64 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 const _ = require('./underscore-min.js');
-
+var router = express.Router();
+var app = express();
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+const HOST = path.join(__dirname, 'host.html');
 
 var users = [];
+express().use(express.static('public'));
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use('/', express.static(__dirname + '/'));
+const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// router.get('/host', function(req, res) {
+//     res.sendFile(HOST);});
 const io = socketIO(server);
 
+var host;
+var players = [];
+var roomcode = 0;
+
+var nextIndex = 0;
 io.on('connection', (socket) => {
     socket.emit('welcome', { message: 'Welcome!', id: socket.id });
     users.push([socket.id, ""]);
     socket.on('i am client', console.log);
-    socket.on('sendData', (data) => dataSent(data));
-});
+    socket.on('i am host',() =>
+    { host = socket;
+        socket.on('disconnectPlayer', (index) => {players[index].disconnect();
+        nextIndex--;
+        })
+        roomcode = Math.floor(Math.random()*10000);
+        socket.emit('roomcreated', {
+            message: roomcode, 
+            id: socket.id
+        });
+    });
+    socket.on('joinRoom', (data) =>{
+        console.log("room code: " + roomcode + "\n code provided: " + data.room)
+        if(data.room == (roomcode+"")){
+            console.log("adding player");
+        io.emit('newPlayer', {name: data.name, index: nextIndex});
+        nextIndex++;
+        players.push(socket);
+        socket.emit("joinedRoom", "success");
+        }
+        else{
+            console.log("bad room code");
+        }
+    })
+    socket.on('sendData', (data) => {
+        dataSent(data);
+})});
+
+
 
 function dataSent(data){
     console.log(data.data.substring(0,4));
